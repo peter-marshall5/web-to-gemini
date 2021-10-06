e7de9b2f2c87ad5982075d45f850776b1114723c
Peter Marshall <petmshall@gmail.com>
2021-10-06 14:11:45 -0400

    Lots of algorithm changes and cleanup

--- geminify.js
+++ geminify.js
@@ -15,71 +15,123 @@ function geminify (article, req) {
   let result = ''
   if (article.title) result += '# ' + article.title + '\n'
   result += analyzePage(pages, req).replace(/^\n/, '')
-  //console.log('Geminify result:', result)
+  // .replace(/[\n]+/g, '\n').replace(/\n[\ ]+/g, '\n')
+  // console.log('Geminify result:', result)
   return result
+}
+
+function isText (text) {
+  if (!text) return false
+  for (let i = 0; i < text.length; i++) {
+    if (text[i] != '\n' && text[i] != ' ') return true
+  }
+  return false
+}
+
+function endsWithWhitespace (text) {
+  return text.endsWith('\n') || text.endsWith(' ')
+}
+
+function startsWithWhitespace (text) {
+  return text.startsWith('\n') || text.startsWith(' ')
+}
+
+function oneLine (text) {
+  return text.replace(/\n/g, '').replace(/[\ ]+/g, ' ').replace(/^\s/, '').replace(/\s$/, '')
+}
+
+function removeWhitespace (text) {
+  return (startsWithWhitespace(text) ? ' ' : '') + text.replace(/[\ ]+/g, ' ').replace(/[\n]+/g, '\n').replace(/\t/g, '').replace(/^\n/, '').replace(/\s$/, '').replace(/\n$/, '').replace(/\s$/, '') + (endsWithWhitespace(text) ? ' ' : '')
+}
+
+function getLinkText (link) {
+  if (link.children[0] && link.children[0].nodeName == 'IMG') return link.children[0].alt
+  if (!link.children[0]) return link.innerHTML
+  if (link.childNodes[0] && link.childNodes[0].nodeValue) return link.childNodes[0].nodeValue
+  return
+}
+
+function addLink (url, text) {
+  return '=> ' + oneLine(url) + ' ' + oneLine(text) + '\n'
+}
+
+function addBullets (lines) {
+  return '* ' + lines.replace(/\n/g, '\n* ') + '\n'
+}
+
+function addHeading (heading) {
+  return '# ' + heading.replace(/\n/g, '\n# ') + '\n'
+}
+
+function addCode (code) {
+  return '```\n' + code.replace(/```/g, '``') + '\n```\n'
+}
+
+function addText (text) {
+  return (newline ? '' : ' ') + removeWhitespace(text)
 }
 
 function analyzePage (page, req) {
   let result = ''
-  for (let i = 0; i < page.childNodes.length; i++) {
-    if (page.childNodes[i].nodeName == 'IMG') {
+  switch (page.nodeName) {
+    case '#text':
+    if (isText(page.nodeValue)) {
+      result += addText(page.nodeValue)
+      newline = result.endsWith('\n')
+    }
+    break
+    case 'IMG':
+    if (!newline) result += '\n'
+    result += addLink(common.getImageSrc(page, req), (page.alt || '[Image]'))
+    newline = true
+    break
+    case 'A':
+    if (!page.href.startsWith('about:')) {
       if (!newline) result += '\n'
-      result += '=> ' + common.getImageSrc(page.childNodes[i], req) + ' ' + (page.childNodes[i].alt || '[Image]').replace(/\n/g, '') + '\n'
+      const parsed = common.parseURL(page.href, req)
+      result += addLink(parsed, (getLinkText(page) || parsed))
       newline = true
     }
-    if (page.childNodes[i].childNodes && page.childNodes[i].childNodes.length > 0) {
-      // console.log('not the end', page.children[i].nodeName, page.children[i].innerHTML)
+    break
+    case 'LI':
+    if (isText(page.innerText)) {
+      console.log('li', page)
+      if (!newline) result += '\n'
+      result += addBullets(page.innerText)
+      newline = true
+      break
+    }
+    case 'H1':
+    case 'H2':
+    case 'H3':
+    if (page.childNodes[0] && isText(page.childNodes[0].textContent)) {
+      if (!newline) result += '\n'
+      if (isText(page.childNodes[0].textContent)) result += addHeading(page.childNodes[0].textContent)
+      newline = true
+    }
+    break
+    case 'CODE':
+    case 'PRE':
+    if (!newline) result += '\n'
+    result += addCode(page.innerHTML || '')
+    newline = true
+    break
+    default:
+    for (let i = 0; i < page.childNodes.length; i++) {
       result += analyzePage(page.childNodes[i], req)
-    } else {
-      switch (page.nodeName) {
-        case 'A':
-        //console.log('Newline for link:', newline)
-        if (!newline) result += '\n'
-        result += '=> ' + common.parseURL(page.childNodes[i].parentNode.href, req) + ' ' + (page.childNodes[i].nodeValue || page.childNodes[i].parentNode.href).replace(/\n/g, '') + '\n'
-        newline = true
-        break
-        case 'LI':
-        if (page.childNodes[i].nodeValue && !/\n/.test(page.childNodes[i].nodeValue)) {
-          if (!newline) result += '\n'
-          result += '* ' + page.childNodes[i].nodeValue.replace(/\n/g, '\n* ') + '\n'
-          newline = true
-        }
-        break
-        case 'H1':
-        case 'H2':
-        case 'H3':
-        if (page.childNodes[i].nodeValue && !/\n/.test(page.childNodes[i].nodeValue)) {
-          if (!newline) result += '\n'
-          result += '# ' + page.childNodes[i].nodeValue.replace(/\n/g, '\n# ') + '\n'
-          newline = true
-        }
-        break
+      switch (page.childNodes[i].nodeName) {
         case 'P':
-        // if (!newline) result += '\n'
-        result += (page.childNodes[i].nodeValue || '').replace(/[\ ]+/g, ' ') + ' '
-        // + '\n'
-        // newline = true
-        newline = result.endsWith('\n')
-        break
-        case 'CODE':
-        if (!newline) result += '\n'
-        result += '```\n' + (page.childNodes[i].nodeValue || '').replace(/```/g, '``') + '\n```\n'
-        newline = true
-        break
-        default:
-        if (page.childNodes[i].nodeValue && page.childNodes[i].nodeValue != '\n') {
-          if (newline) {
-            result += page.childNodes[i].nodeValue.replace(/\n/, '').replace(/[\n]+/g, '\n').replace(/\t/g, '').replace(/[\ ]+/g, ' ')
-          } else {
-            result += page.childNodes[i].nodeValue.replace(/[\n]+/g, '\n').replace(/\t/g, '').replace(/[\ ]+/g, ' ')
-          }
-          newline = result.endsWith('\n')
+        case 'FIGURE':
+        // Exited from paragraph
+        if (!newline) {
+          result += '\n'
+          newline = true
         }
       }
-      //console.log('end', page.childNodes[i].childNodes.length, page.childNodes[i].nodeValue)
     }
   }
-  return result.replace(/[\n]+/g, '\n').replace(/\n[\ ]+/g, '\n')
+  return result
+  // .replace(/[\n]+/g, '\n').replace(/\n[\ ]+/g, '\n')
 }
 
 module.exports = geminify
